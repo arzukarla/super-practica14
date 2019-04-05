eb87628e71b51ce849ff1e509b95fa6248cd10f1
arzukarla <48573856+arzukarla@users.noreply.github.com>
2019-04-05 13:38:38 +0300

    lab4

---
+++ lab4/lab4/lab4/lab4.cpp
@@ -0,0 +1,66 @@
+// lab4.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Child {
+private:
+	string name;
+	string last_name;
+	int age;
+
+public:
+  Child(string, string, int);
+
+	~Child();
+	
+	void setName(string n) { name = n; }
+	void setLast_name(string l) { last_name = l; }
+	void setAge(int a) { age = a; }
+
+	string getName() { return name; }
+	string getLast_name() { return last_name; }
+	int getAge() { return age; }
+
+};
+//constructor
+Child::Child(string, string, int)
+{
+	name = name;
+	last_name = last_name;
+	age = age;
+}
+//Destructor
+Child::~Child(){
+}
+
+int main()
+{
+	Child Dates;
+	Dates.setName("Karla");
+	Dates.setLast_name("Arzu");
+	Dates.setAge(15);
+	cout << Dates.getName() << endl;
+	cout << Dates.getLast_name() << endl;
+	cout << Dates.getAge() << endl;
+	return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+
+
+
+
+
