57862e0981f09c850b01379dfb8539679bdd4bc7
arzukarla <48573856+arzukarla@users.noreply.github.com>
2019-05-17 05:11:26 +0300

    lab4.45

---
+++ lab4.45/Child.1/Child.1/Child.1.cpp
@@ -0,0 +1,58 @@
+// Child.1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Child {
+
+private:
+	string name;
+	string surname;
+	int age;
+
+public:
+	Child(string, string, int);//Constructor
+	~Child();//Destructor
+	void mostrarDatos();
+};
+
+//constructor
+Child::Child(string _name, string _surname, int _age) {
+	name = _name;
+	surname = _surname;
+	age = _age;
+}
+	//Destructor
+	Child::~Child() {
+
+	}
+
+	void Child::mostrarDatos()
+	{
+		cout << "name:" << name << endl;
+		cout <<"surname:"<< surname << endl;
+		cout << "age" << '.' << endl;
+	}
+
+	
+int main()
+{
+	Child Date("Carlitos", "Ramirez", 25);
+	Date.mostrarDatos();
+	cout << "\n";
+	return 0;
+}
+
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
